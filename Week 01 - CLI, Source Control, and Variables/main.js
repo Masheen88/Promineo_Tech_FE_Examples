27d82de4ef29febe48fccaba5451ed6ee8e328f2
Matthew Cox <mattdrummerguy@gmail.com>
2022-08-06 12:24:02 -0500

    updated example

---
+++ Week 01 - CLI, Source Control, and Variables/main.js
@@ -0,0 +1,24 @@
+console.log("test");
+// add team scores together
+
+//step 1: need to create variables
+
+//Describe what your variable is for
+// This is the score for team 1
+let team1score = 25;
+
+/* Leave a console log with strings of text describing a short summary of 
+what you expect to see and then log the variable.
+*/
+console.log("This is the score for team 1:", team1score);
+
+//Update your comments and console logs as your code evolves.
+
+// This is the score for team 2
+let team2score = 15;
+console.log("This is the score for team 2:", team2score);
+
+//step 2: need to add the scores together
+
+// This is the scores for team 1 and team 2 added together
+console.log("This is the score for both teams:", team2score + team1score);
